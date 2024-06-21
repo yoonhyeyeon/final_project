35ac28654b1d452c0ccc10103ab8e12fe98c5371
EUNJI\eunji <eunjii_0311@naver.com>
2024-06-21 14:27:27 +0900

    프론트 수정 사항 업데이트

---
+++ practice_SHIN/frontWorkspace/groupware/js/adminUtil.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('./header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('headerContainer').innerHTML = data;
+        });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('./nav.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navContainer').innerHTML = data;
+        });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('./footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footerContainer').innerHTML = data;
+    });
+})

02e0eef87c73e36bddf8f9422f7fcf10b4a5b115
DESKTOP-CBR3J1J\user1 <>
2024-07-08 18:33:51 +0900

    projectList 중복 1줄삭제

--- eyesOnUs/app/src/main/resources/static/js/projectJs/projectList.js
+++ eyesOnUs/app/src/main/resources/static/js/projectJs/projectList.js
@@ -14,7 +14,6 @@ $.ajax({
             str += "<td>" + voList[i].state + "</td>";
             str += "<td>" + voList[i].startDate + "</td>";
             str += "<td>" + voList[i].modifyDate + "</td>";
-            str += "<td>" + voList[i].modifyDate + "</td>";
             str += "</tr>";
         }
         tbody.innerHTML = str;

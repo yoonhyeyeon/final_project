fcfead3fbe3894892b4317d73cb80cf2aea33454
DESKTOP-40T4D40\user1 <>
2024-07-18 19:31:23 +0900

    출장 만회

--- eyesOnUs/app/src/main/java/com/kh/app/sign/service/SignService.java
+++ eyesOnUs/app/src/main/java/com/kh/app/sign/service/SignService.java
@@ -1,5 +1,6 @@
 package com.kh.app.sign.service;
 
+import com.kh.app.member.vo.MemberVo;
 import com.kh.app.sign.dao.SignDao;
 import com.kh.app.sign.vo.EmployeeVo;
 import com.kh.app.sign.vo.SignVo;
@@ -7,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @Transactional
@@ -28,7 +31,7 @@ public class SignService {
             signSeq++;
         }
 
-        int refResult = 1;
+        int refResult = 0;
         if(signVo.getRefNo() != null){
             for (String ref : signVo.getRefNo()) {
                 refResult = dao.signRefWrite(ref);
@@ -39,48 +42,98 @@ public class SignService {
     } // signWrite
 
     // 직원 전체 목록 조회 (API)
-    public List<EmployeeVo> getEmployeeList() {
-        return dao.getEmployeeList();
+    public Map<String, List> getEmployeeList() {
+        // 직원 전체 목록 조회 (API)
+        List<EmployeeVo> employeeVoList = dao.getEmployeeList();
+
+        // 직원 전체 목록 묶기
+        Map<String, List> employeeListMap = new HashMap<>();
+        employeeListMap.put("employeeVoList", employeeVoList);
+
+        return employeeListMap;
     } // getEmployeeList
 
     // 결재 목록 조회 (기안자 입장) (API)
-    public List<SignVo> getSignListForWriter(String empNo) {
-        return dao.getSignListForWriter(empNo);
+    public Map<String, List> getSignListForWriter(String empNo) {
+        // 결재 목록 조회 (기안자 입장) (API)
+        List<SignVo> signVoListForWriter = dao.getSignListForWriter(empNo);
+
+        // 결재 목록 묶기
+        Map<String, List> signListMapForWriter = new HashMap<>();
+        signListMapForWriter.put("signVoListForWriter", signVoListForWriter);
+
+        return signListMapForWriter;
     } // getSignListForWriter
 
     // 결재 목록 조회 (결재자 입장) (API)
-    public List<SignVo> getSignListForApprover(String approverNo) {
-        return dao.getSignListForApprover(approverNo);
+    public Map<String, List> getSignListForApprover(String approverNo) {
+        // 결재 목록 조회 (결재자 입장) (API)
+        List<SignVo> signVoListForApprover = dao.getSignListForApprover(approverNo);
+
+        // 결재 목록 묶기
+        Map<String, List> signListMapForApprover = new HashMap<>();
+        signListMapForApprover.put("signVoListForApprover", signVoListForApprover);
+
+        return signListMapForApprover;
     } // getSignListForApprover
 
     // 결재 목록 조회 (참조자 입장) (API)
-    public List<SignVo> getSignListForReference(String refNo) {
-        return dao.getSignListForReference(refNo);
+    public Map<String, List> getSignListForReference(String refNo) {
+        // 결재 목록 조회 (참조자 입장) (API)
+        List<SignVo> signVoListForReference = dao.getSignListForReference(refNo);
+
+        // 결재 목록 묶기
+        Map<String, List> signListMapForReference = new HashMap<>();
+        signListMapForReference.put("signVoListForReference", signVoListForReference);
+        
+        return signListMapForReference;
     } // getSignListForReference
 
     // 결재 상세 조회 (API)
-    public SignVo getSignDetail(SignVo signVo) {
-        return dao.getSignDetail(signVo);
+    public Map<String, SignVo> getSignDetail(SignVo signVo, MemberVo loginMemberVo) {
+        // 결재 상세 조회 (API)
+        SignVo signDetailVo = dao.getSignDetail(signVo);
+
+        // 승인, 반려 버튼 판단용 로그인 사원 번호
+        SignVo vo = new SignVo();
+        vo.setEmpNo(loginMemberVo.getNo());
+
+        // 결재 상세, 로그인 사원 번호 묶기
+        Map<String, SignVo> signDetailMap = new HashMap<>();
+        signDetailMap.put("signDetailVo", signDetailVo);
+        signDetailMap.put("vo", vo);
+
+        return signDetailMap;
     } // getSignDetail
 
     // 결재자 상세 조회 (API)
-    public List<SignVo> getSignApproverDetailList(SignVo signVo) {
-        return dao.getSignApproverDetailList(signVo);
+    public Map<String, List> getSignApproverDetailList(SignVo signVo) {
+        // 결재자 상세 조회 (API)
+        List<SignVo> signApproverDetailVoList = dao.getSignApproverDetailList(signVo);
+
+        // 참조자 상세 조회 (API)
+        List<SignVo> signReferenceDetailVoList = dao.getSignReferenceDetailList(signVo);
+
+        // 결재자 상세, 참조자 상세 묶기
+        Map<String, List> signApproverAndReferenceDetailMap = new HashMap<>();
+        signApproverAndReferenceDetailMap.put("signApproverDetailVoList", signApproverDetailVoList);
+        signApproverAndReferenceDetailMap.put("signReferenceDetailVoList", signReferenceDetailVoList);
+
+        return signApproverAndReferenceDetailMap;
     } // getSignApproverDetailList
 
-    // 참조자 상세 조회 (API)
-    public List<SignVo> getSignReferenceDetailList(SignVo signVo) {
-        return dao.getSignReferenceDetailList(signVo);
-    } // getSignReferenceDetailList
-
     // 결재 (API)
-    public int updateSignApprove(SignVo signVo) {
+    public Map<String, Integer> updateSignApprove(SignVo signVo) {
+        // 결재 (API)
         int signResult = dao.updateSign(signVo);
         int fileResult = dao.updateFile(signVo);
         int commentResult = dao.writeComment(signVo);
 
+        // 결재 정보 묶기
         int signApproveResult = signResult * fileResult * commentResult;
+        Map<String, Integer> signApproveResultMap = new HashMap<>();
+        signApproveResultMap.put("signApproveResult", signApproveResult);
 
-        return signApproveResult;
+        return signApproveResultMap;
     } // updateSignApprove
 } // class
